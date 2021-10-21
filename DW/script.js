eb56f2f911787eada1eb6c22973e31f9f151db34
lucia-sebe <luli.se04@gmail.com>
2021-10-21 08:36:38 -0300

    21/10 8:40 Lu

--- DW/script.js
+++ DW/script.js
@@ -240,12 +240,8 @@ const pasapalabra = () => {
 // se llama en la linea 159 (windo.onload)
 function reloj() {
     fecha = new Date(); //Actualizar fecha.
-    hora = fecha.getHours(); //hora actual
     minuto = fecha.getMinutes(); //minuto actual
     segundo = fecha.getSeconds(); //segundo actual
-    if (hora < 10) { //dos cifras para la hora
-        hora = "0" + hora;
-    }
     if (minuto < 10) { //dos cifras para el minuto
         minuto = "0" + minuto;
     }
@@ -257,13 +253,8 @@ function reloj() {
     console.log(mireloj);
 }
 
-
-
 //LISTA DE TAREAS
 //array de respuestas
-//nombre de las variables
-//autofocus luego del boton enviar
-//mostrar los contadores
 
 //categorias:
 //no hace falta que sean muchas materias, economia y una mas (tutores o estudiantes, algo original)
