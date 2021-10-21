530225c58dd6be558080f252f84e3ba53cb19292
camila-ctrl <camilaperezvazquez142@gmail.com>
2021-10-21 17:28:47 -0300

    archivos de juan ordenados y facil acceso para fijarnos

---
+++ Prog/COSAS DE JUAN/restservice/LOGIC/ManejadorUser.java
@@ -0,0 +1,49 @@
+//MANEJADORUSER DE JUAN (REST SERVICE - EJEMPLO)
+package com.pasapalabraapi.logic;
+
+import java.util.ArrayList;
+
+import com.pasapalabraapi.persistence.ManejadorBD;
+
+public class ManejadorUser {
+	
+	private ManejadorBD manejadorBD = new ManejadorBD();
+	
+	public Usuario createJugador(int ciA, int pinA, String nombre, int ci, int pin) throws Exception {
+		if(manejadorBD.validateCredentialsAdmin(ciA, pinA)) {
+			return manejadorBD.createJugador(new Jugador(nombre, ci, pin));
+		} else {
+			return null;
+		}
+	}
+	
+	public Usuario createAdmin(int ciA, int pinA, String nombre, int ci, int pin) throws Exception {
+		if(manejadorBD.validateCredentialsAdmin(ciA, pinA)) {
+			return manejadorBD.createAdmin(new Administrador(nombre, ci, pin));
+		} else {
+			return null;
+		}
+	}
+	
+	public Usuario login(int ci, int pin) throws Exception {
+		return manejadorBD.validateCredentials(ci, pin);
+	}
+	
+	public ArrayList<Usuario> getJugadores() {
+		return manejadorBD.getJugadores();
+	}
+	
+	public ArrayList<Usuario> getAdministradores() {
+		return manejadorBD.getAdministradores();
+	}
+
+	public void deleteUser(int jugadorId) throws Exception {
+		manejadorBD.deleteUser(jugadorId);
+		
+	}
+	
+	public void deleteAdmin(int jugadorId) throws Exception {
+		manejadorBD.deleteAdmin(jugadorId);
+		
+	}
+}
