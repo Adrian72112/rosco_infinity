d3f36b1de938d543237c0e975811c0baec473f73
camila <camilaperezvazquez142@gmail.com>
2021-10-28 12:02:02 -0300

    DB Conf y cosas de java

---
+++ Prog/InfinityProyect/src/persistence/DBConf.java
@@ -0,0 +1,48 @@
+package persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConf {
+	// Librera de MySQL
+    public String driver = "com.mysql.cj.jdbc.Driver";
+
+    // Nombre de la base de datos
+    public String database = "Infinity"; //NOMBRE DE NUESTRA BASE DE DATOS
+
+    // Host
+    public String hostname = "192.168.64.2";
+
+    // Puerto
+    public String port = "3306";
+
+    // Ruta de nuestra base de datos (desactivamos el uso de SSL con "?useSSL=false")
+    public String url = "jdbc:mysql://" + hostname + ":" + port + "/" + database + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+    //CAMI
+    // Nombre de usuario
+    public String username = "root";
+
+    // Clave de usuario
+    public String password = "";
+
+    //LULU - TENES QUE HACER UN USUARIO
+   // public String username = "root";
+
+    // Clave de usuario
+   // public String password = "";
+
+    public Connection conectarMySQL() {
+        Connection conn = null;
+
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+}
