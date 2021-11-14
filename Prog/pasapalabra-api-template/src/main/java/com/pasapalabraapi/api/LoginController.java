71da23c95bc94a7f67f5e94edeaa48c86e660e7b
Adrian Tesore <adrian72112@gmail.com>
2021-11-14 01:24:11 -0300

    API CONTROLLER

--- Prog/pasapalabra-api-template/src/main/java/com/pasapalabraapi/api/LoginController.java
+++ Prog/pasapalabra-api-template/src/main/java/com/pasapalabraapi/api/LoginController.java
@@ -1,24 +1,33 @@
 package com.pasapalabraapi.api;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.server.ResponseStatusException;
-
 import com.pasapalabraapi.DTO.CreateUsuarioDTO;
 import com.pasapalabraapi.DTO.LoginDTO;
 import com.pasapalabraapi.logic.ManejadorUsuario;
 import com.pasapalabraapi.logic.Usuario;
 
+@Controller
 public class LoginController {
 	
 	private ManejadorUsuario manejadorUsuario = new ManejadorUsuario();
 	
 	@PostMapping("/login")
 	@ResponseBody
-	public Usuario login(@RequestBody(required = true) LoginDTO loginDTO) {
-		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Not implemented...");
+	public boolean login(@RequestBody(required = true) LoginDTO loginDTO) {
+		try {
+			if (manejadorUsuario.validarUsuario(loginDTO.getCi(), loginDTO.getPin()) == true) {
+				return true;
+			} else {
+				return false;
+			}
+		}catch(Exception e) {
+			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Usuario no autorizado para login");
+		}
 	} 
 	
 	@PostMapping("/user")
